1d1ceb2b9cecc36b412ea03bcb7e163def2d7b6a
Muhindo Kiro <blaize1143@gmail.com>
2022-05-13 22:31:00 +0300

    generate app routing and app module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layout/default/default.component';
+
+const moduleRoutes: Routes = [
+  {
+    path: 'home',
+    // canActivate: [AuthGuard],
+    loadChildren: () =>  import('./views/home/home.module').then(m => m.HomeModule)
+  },
+]
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: DefaultComponent,
+    children: moduleRoutes
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
