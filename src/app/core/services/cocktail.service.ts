8f0044761e223a2c59f3a18d972e1819f5dee809
Muhindo Kiro <blaize1143@gmail.com>
2022-05-16 02:59:36 +0300

    chages

--- src/app/core/services/cocktail.service.ts
+++ src/app/core/services/cocktail.service.ts
@@ -15,11 +15,11 @@ export class CocktailService {
   constructor(private http: HttpClient) { }
 
   getCocktails(): Observable<any> {
-    // return this.http.get(this.baseUrl + '/drinks/');
-        return this.http.get('base_url:"https://murmuring-bayou-72324.herokuapp.com/drinks')
+    return this.http.get(this.baseUrl + '/drinks/');
+        // return this.http.get('https://murmuring-bayou-72324.herokuapp.com/drinks')
   }
     addCockTail(data: any): Observable<any> {
-    // return this.http.post(this.baseUrl + '/drinks/',data)
-    return this.http.post('base_url:"https://murmuring-bayou-72324.herokuapp.com/drinks',data)
+    return this.http.post(this.baseUrl + '/drinks/',data)
+    // return this.http.post('https://murmuring-bayou-72324.herokuapp.com/drinks',data)
   }
 }
