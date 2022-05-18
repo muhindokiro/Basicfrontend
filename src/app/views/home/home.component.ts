329c25f23dcd5752f737ca4b8566e891ea2342e2
Muhindo Kiro <blaize1143@gmail.com>
2022-05-18 23:09:57 +0300

    create method to get data from local database

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,26 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import random from '@angular/cli'
 import { CocktailService } from 'src/app/core/services/cocktail.service';
+import { LocaldrinksService } from 'src/app/core/services/localdrinks.service';
 // import {ToastrService} from 'ngx-toastr';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+  loadingDrinks = false;
   loadingCocktails = false;
   cocktailList: any[] = [];
+  drinksList: any[] = [];
   loadingPost=false
   cockTailForm!: FormGroup;
   constructor(
     private cocktailService:CocktailService,
+    private localdrinkService: LocaldrinksService,
     private router:Router,
     // private toaster:ToastrService
   ) { }
 
   ngOnInit(): void {
     this.getCocktails()
+    this.getDrinks()
 
     this.cockTailForm = new FormGroup({
       name: new FormControl(''),
@@ -30,6 +38,8 @@ export class HomeComponent implements OnInit {
 
     })
   }
+
+  //Get 5 coktails from cocktail api
   getCocktails() {
     this.loadingCocktails = true
     this.cocktailService.getCocktails().subscribe((data)=>{
@@ -40,6 +50,33 @@ export class HomeComponent implements OnInit {
       this.loadingCocktails=false
     })
 }
+
+//Get cocktails from local database
+getDrinks(){
+  this.loadingDrinks = true
+    this.localdrinkService.getDrinks().subscribe((data)=>{
+      let drinks = data.drink;
+      const size = 1
+      const items = drinks.slice(0, size)
+      this.drinksList = items
+      this.loadingDrinks=false
+    })
+}
+
+// getRandomDrink() {
+//   this.cocktailService.getCocktails().subscribe((data)=>{
+//       let randomdrinks = data.drinks;
+//       let randomdrink = 5
+//       while randomdrink > 0:
+//       items = randomdrinks.random.randint(0, len(randomdrinks))
+//       // this.cocktailList = items
+//       print (randomdrinks[items])
+//       randomdrink -= 1
+//       this.loadingCocktails=false
+//     })
+// }
+
+//Add cocktail,,refresh page from new data to display
 addCockTail(){
 const payload={
   name:this.cockTailForm.get('name')?.value,
@@ -60,3 +97,7 @@ console.log(payload,"THE PAYLOAD WE EXPECT!!");
         });
 }
 }
+function len(data: any): any {
+  throw new Error('Function not implemented.');
+}
+
